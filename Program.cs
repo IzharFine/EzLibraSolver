9679c8efe54668ce701417cd2d3878001f7d366d
Izhar Fine <izharf@gmail.com>
2020-02-17 10:05:05 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,90 +6,127 @@ namespace HackRank
 {
     class Program
     {
+        private static Dictionary<LibraEnum, int> _sumHelper = new Dictionary<LibraEnum, int>();
+        private static bool _isFinished = false;
+
         static void Main(string[] args)
         {
             Queue<int> weightStack = new Queue<int>();
             Console.WriteLine("Enter your 'RL' string:");
             string rightLeftString = Console.ReadLine();
             Dictionary<LibraEnum, Stack<int>> libra = new Dictionary<LibraEnum, Stack<int>>();
-            libra = InitLibra(libra);
-            for (int i = 1 ; i <= rightLeftString.Length; i++)
+            InitLibra(libra, _sumHelper);
+            for (int i = 1; i <= rightLeftString.Length; i++)
             {
                 weightStack.Enqueue(i);
             }
             SolveLibra(weightStack, libra, rightLeftString, 0);
+            PrintLibra(libra, weightStack.Count);
         }
 
-        private static Dictionary<LibraEnum, Stack<int>> InitLibra(Dictionary<LibraEnum, Stack<int>> libra)
+        private static void InitLibra(Dictionary<LibraEnum, Stack<int>> libra, Dictionary<LibraEnum, int> sumHelper)
         {
             libra[LibraEnum.LEFT_SIDE] = new Stack<int>();
             libra[LibraEnum.RIGHT_SIDE] = new Stack<int>();
             libra[LibraEnum.LEFT_INDEX] = new Stack<int>();
             libra[LibraEnum.RIGHT_INDEX] = new Stack<int>();
-            libra[LibraEnum.IS_FINISHED] = new Stack<int>();
-            return libra;
+
+            _sumHelper.Add(LibraEnum.RIGHT_SIDE_SUM, 0);
+            _sumHelper.Add(LibraEnum.LEFT_SIDE_SUM, 0);
         }
 
-        private static void SolveLibra(Queue<int> weightStack, Dictionary<LibraEnum, Stack<int>> libra, string rightLeftString, int counter)
+        private static void SolveLibra(
+            Queue<int> weightStack,
+            Dictionary<LibraEnum, Stack<int>> libra,
+            string rightLeftString,
+            int counter)
         {
-            for(int i=0;i< weightStack.Count && libra[LibraEnum.IS_FINISHED].Count == 0; i++)
+            for (int i = 0; i < weightStack.Count && !_isFinished; i++)
             {
                 int weight = weightStack.Dequeue();
                 LibraEnum sideNeedToBeHeavy = CharToLibraSideEnum(rightLeftString[0]);
-                if (IsValidMove(libra, sideNeedToBeHeavy, LibraEnum.LEFT_SIDE, weight))
+                if (IsValidMove(libra, sideNeedToBeHeavy, GetOppositeEnum(sideNeedToBeHeavy), weight))
                 {
-                    HandleLibraMove(LibraEnum.LEFT_SIDE, weight, weightStack, libra, rightLeftString, counter);
+                    HandleLibraMove(GetOppositeEnum(sideNeedToBeHeavy), weight, weightStack, libra, rightLeftString, counter);
                 }
-                if (IsValidMove(libra, sideNeedToBeHeavy, LibraEnum.RIGHT_SIDE, weight))
+                if (!_isFinished && IsValidMove(libra, sideNeedToBeHeavy, sideNeedToBeHeavy, weight))
                 {
-                    HandleLibraMove(LibraEnum.RIGHT_SIDE, weight, weightStack, libra, rightLeftString, counter);
+                    HandleLibraMove(sideNeedToBeHeavy, weight, weightStack, libra, rightLeftString, counter);
                 }
                 weightStack.Enqueue(weight);
             }
         }
 
-        private static void HandleLibraMove(LibraEnum side,
-            int weight, Queue<int> weightStack,
+        private static void HandleLibraMove(
+            LibraEnum side,
+            int weight,
+            Queue<int> weightStack,
             Dictionary<LibraEnum, Stack<int>> libra,
             string rightLeftString,
             int counter)
         {
-            libra[side].Push(weight);
-            libra[GetIndexBySide(side)].Push(counter);
+            HandleLibraPush(side, weight, libra, counter);
+
             if (!weightStack.Any())
             {
-                libra[LibraEnum.IS_FINISHED].Push(1);
+                _isFinished = true;
                 return;
             }
+
             SolveLibra(weightStack, libra, rightLeftString.Substring(1, rightLeftString.Length - 1), ++counter);
-            if (libra[LibraEnum.IS_FINISHED].Count == 0)
+
+            if (!_isFinished)
             {
-                libra[side].Pop();
-                libra[GetIndexBySide(side)].Pop();
+                HandleLibraPop(side, weight, libra);
             }
         }
 
-        private static LibraEnum CharToLibraSideEnum(char chr)
+        private static void HandleLibraPush(LibraEnum side, int weight, Dictionary<LibraEnum, Stack<int>> libra, int counter)
         {
-            switch (chr)
-            {
-                case 'R':
-                    return LibraEnum.RIGHT_SIDE;
-                case 'L':
-                    return LibraEnum.LEFT_SIDE;
-            }
-            return LibraEnum.ERROR;
+            libra[side].Push(weight);
+            libra[GetIndexBySide(side)].Push(counter);
+            _sumHelper[GetSumEnumBySide(side)] += weight;
         }
 
-        private static bool IsValidMove(Dictionary<LibraEnum, Stack<int>> libra, LibraEnum sideNeedToBeHeavy, LibraEnum addToSide, int weight)
+        private static void HandleLibraPop(LibraEnum side, int weight, Dictionary<LibraEnum, Stack<int>> libra)
+        {
+            libra[side].Pop();
+            libra[GetIndexBySide(side)].Pop();
+            _sumHelper[GetSumEnumBySide(side)] -= weight;
+        }
+
+        private static bool IsValidMove(
+            Dictionary<LibraEnum, Stack<int>> libra,
+            LibraEnum sideNeedToBeHeavy,
+            LibraEnum addToSide,
+            int weight)
         {
             return sideNeedToBeHeavy == GetHeavySide(libra, addToSide, weight);
         }
 
+        private static void PrintLibra(Dictionary<LibraEnum, Stack<int>> libra, int count)
+        {
+            Console.WriteLine("\r\nL \t R");
+            for (int i = count - 1; i >= 0; i--)
+            {
+                if (libra[LibraEnum.RIGHT_INDEX].Count != 0 && libra[LibraEnum.RIGHT_INDEX].Peek() == i)
+                {
+                    libra[LibraEnum.RIGHT_INDEX].Pop();
+                    Console.WriteLine($"\t {libra[LibraEnum.RIGHT_SIDE].Pop()}");
+                }
+                else
+                {
+                    libra[LibraEnum.LEFT_INDEX].Pop();
+                    Console.WriteLine($"{libra[LibraEnum.LEFT_SIDE].Pop()}");
+                }
+            }
+            Console.ReadLine();
+        }
+
         private static LibraEnum GetHeavySide(Dictionary<LibraEnum, Stack<int>> libra, LibraEnum addToSide, int weight)
         {
-            int addToSideSum = libra[addToSide].Sum() + weight;
-            int oppositeSideSum = libra[GetOppositeEnum(addToSide)].Sum();
+            int addToSideSum = _sumHelper[GetSumEnumBySide(addToSide)] + weight;
+            int oppositeSideSum = _sumHelper[GetSumEnumBySide(GetOppositeEnum(addToSide))];
 
             if (addToSideSum > oppositeSideSum)
             {
@@ -110,6 +147,23 @@ namespace HackRank
             return (side == LibraEnum.RIGHT_SIDE) ? LibraEnum.RIGHT_INDEX : LibraEnum.LEFT_INDEX;
         }
 
+        private static LibraEnum GetSumEnumBySide(LibraEnum side)
+        {
+            return (side == LibraEnum.RIGHT_SIDE) ? LibraEnum.RIGHT_SIDE_SUM : LibraEnum.LEFT_SIDE_SUM;
+        }
+
+        private static LibraEnum CharToLibraSideEnum(char chr)
+        {
+            switch (chr)
+            {
+                case 'R':
+                    return LibraEnum.RIGHT_SIDE;
+                case 'L':
+                    return LibraEnum.LEFT_SIDE;
+            }
+            return LibraEnum.ERROR;
+        }
+
         private enum LibraEnum
         {
             RIGHT_SIDE,
@@ -117,8 +171,9 @@ namespace HackRank
             EQUAL,
             RIGHT_INDEX,
             LEFT_INDEX,
-            IS_FINISHED,
-            ERROR
+            ERROR,
+            LEFT_SIDE_SUM,
+            RIGHT_SIDE_SUM
         }
     }
 }
